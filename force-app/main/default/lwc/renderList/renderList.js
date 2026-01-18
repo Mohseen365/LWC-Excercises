0ea4ed3d526a438feb10f5693e9d7fe2852c1c4a
Mohseen365 <mohammedmohsin365@gmail.com>
2026-01-18 14:48:27 +0530

    renderList

---
+++ force-app/main/default/lwc/renderList/renderList.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class RenderList extends LightningElement {
+    contacts = [
+        {
+            Id: 1,
+            Name: 'Mohseen',
+            Title: 'Salesforce Dev'
+        },
+        {
+            Id: 2,
+            Name: 'Ali',
+            Title: 'Salesforce Admin'
+        },
+        {
+            Id: 3,
+            Name: 'Aini',
+            Title: 'Salesforce Consultant'
+        },
+    ];
+    
+}
