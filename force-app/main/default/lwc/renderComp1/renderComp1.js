32e3c64e39371c8c63259ab32c802f778a8c3153
Mohseen365 <mohammedmohsin365@gmail.com>
2026-01-18 14:27:46 +0530

    render on variables

---
+++ force-app/main/default/lwc/renderComp1/renderComp1.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class RenderComp1 extends LightningElement {
+    isBlue = true;
+    isGreen = false;
+    isRed = true;
+    isOrange = true;
+}
